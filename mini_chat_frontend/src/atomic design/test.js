24cee5cb949ba10378896c06915ba4db010e3888
Minas-shannak <minas.shannak200@gmail.com>
2025-05-01 19:07:08 +0300

    Contact data

--- mini_chat_frontend/src/atomic design/test.js
+++ mini_chat_frontend/src/atomic design/test.js
@@ -1,12 +1,22 @@
-import { createContactItem } from "./components/molecules/ChatSidebar/ContactItem/index.js";
+import { createChatSidebar } from "./components/organisms/Chat/Sidebar/index.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const contact = createContactItem({
+const randomColor= ['#4cd964','#5ac8fa','#ff2d55','#bb86fc','#007aff']
+const contacts = [
+  {
     name: 'Melody Macy',
     email: 'melody@altbox.com',
     time: '20 hrs',
-    color: '#4caf50'
-  });
+    color: randomColor[Math.floor(Math.random() * randomColor.length)]
+  },
+  {
+    name: 'Farah N.',
+    email: 'farah@domain.com',
+    time: '5 hrs',
+    color: randomColor[Math.floor(Math.random() * randomColor.length)]
+  }
+];
 
-  document.getElementById('apptest').appendChild(contact);
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebar = createChatSidebar(contacts);
+  document.getElementById('apptest').appendChild(sidebar);
 });
