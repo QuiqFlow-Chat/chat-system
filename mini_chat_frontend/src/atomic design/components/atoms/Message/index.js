29e26c74991092caea77646e649dd2f713510d13
Minas-shannak <minas.shannak200@gmail.com>
2025-05-03 15:30:37 +0300

    MessengerChat

--- mini_chat_frontend/src/atomic design/components/atoms/Message/index.js
+++ mini_chat_frontend/src/atomic design/components/atoms/Message/index.js
@@ -1,19 +1,18 @@
 class MessageText {
-    constructor(textElement) {
-      this.textElement = textElement;
-    }
-  
-    setText(text) {
-      this.textElement.textContent = text;
-      return this;
-    }
+  constructor(textElement) {
+    this.textElement = textElement;
   }
-  
-  export function createMessageText(text = '') {
-    const div = document.createElement('div');
-    div.className = 'message-text';
-    div.textContent = text;
-  
-    return new MessageText(div);
+
+  setText(text) {
+    this.textElement.innerHTML = text.replace(/\n/g, "<br>");
+    return this;
   }
-  
+}
+
+export function createMessageText(text = '') {
+  const div = document.createElement('div');
+  div.className = 'message-text';
+  div.innerHTML = text.replace(/\n/g, "<br>");
+
+  return new MessageText(div);
+}
